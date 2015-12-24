c52b107c5265237a02707476611be66f04647f8e
David Koontz <david@koontzfamily.org>
2015-12-24 12:12:15 -0700

    demonstrate Purescript / JS interop and how PS leaves JS objects 'as-is'

--- purescript/src/Immutable/Test.js
+++ purescript/src/Immutable/Test.js
@@ -6,4 +6,4 @@ const List = require('immutable').List;
 exports.plusNine = function (num) { return 9 + num; };
 // exports.makeList = function() { return exports.mod([1,2,3]); };
 exports.makeList = function() { return List([1,2,3]); };
-exports.length = function(list) { return list.length; };
+exports.length = function(list) { return list.count(); };
